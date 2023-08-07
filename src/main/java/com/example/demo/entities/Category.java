6faeabded5a27bee3e14a6c4717427f2e68a6f13
gparizotto <g.parizotto@unesp.br>
2023-08-07 13:58:51 -0300

    added category and product

--- src/main/java/com/example/demo/entities/Category.java
+++ src/main/java/com/example/demo/entities/Category.java
@@ -1,9 +1,9 @@
 package com.example.demo.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -11,36 +11,30 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="tb_user")
-public class User implements Serializable {
+@Table(name = "tb_category")
+public class Category implements Serializable{
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String email;
-	public String phone;
-	private String password;
 	
 	@JsonIgnore
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
-
-	public User() {
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+	
+	public Category() {
+		
 	}
 
-	public User(Long id, String name, String email, String phone, String password) {
-		super();
+	public Category(Long id, String name) {
 		this.id = id;
 		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.password = password;
 	}
 
 	public Long getId() {
@@ -58,25 +52,9 @@ public class User implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 	
-	public List<Order> getOrders() {
-		return orders;
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
@@ -92,7 +70,7 @@ public class User implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
 }
